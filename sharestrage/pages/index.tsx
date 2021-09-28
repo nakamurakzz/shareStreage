091491c6f2a311eb51d53607dbc8194af0198e07
nkmrkz <kazutaka.nakamura@ncdc.co.jp>
2021-09-28 18:25:04 +0900

    サンプル追加

--- sharestrage/pages/index.tsx
+++ sharestrage/pages/index.tsx
@@ -1,7 +1,28 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
+
 const HomePage: NextPage = () => {
+  const [clickCount,setClickCount]=useState(0)
+
+  const countUp = ()=>{
+    const count = clickCount + 1
+    setClickCount(count)
+  }
+
+  const countDown = ()=>{
+    const count = clickCount - 1
+    setClickCount(count)
+  }
+
   return (
-    <div>Welcome to Next.js.</div>
+    <>
+      <div style={{margin:20}}>
+        <button onClick={countUp}>count up</button>
+        <button onClick={countDown}>count down</button>
+        <div>count：{clickCount}</div>
+      </div>
+    </>
+    
   )
 }
 
